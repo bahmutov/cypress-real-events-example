a3960510c84786979a7f8603f89c1e61e00ac4f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-05 14:17:10 -0400

    skip on firefox

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,16 @@
 import 'cypress-real-events/support'
 
-it('shows the copy code button', { scrollBehavior: 'center' }, () => {
-  cy.visit('dmtrKovalenko/cypress-real-events')
-  cy.contains('h2', 'Installation').scrollIntoView()
+it(
+  'shows the copy code button',
+  { browser: '!firefox', scrollBehavior: 'center' },
+  () => {
+    cy.visit('dmtrKovalenko/cypress-real-events')
+    cy.contains('h2', 'Installation').scrollIntoView()
 
-  cy.contains('.snippet-clipboard-content', 'npm install').within(() => {
-    cy.get('[aria-label=Copy]').should('not.be.visible')
-    cy.root().realHover()
-    cy.get('[aria-label=Copy]').should('be.visible').click()
-  })
-})
+    cy.contains('.snippet-clipboard-content', 'npm install').within(() => {
+      cy.get('[aria-label=Copy]').should('not.be.visible')
+      cy.root().realHover()
+      cy.get('[aria-label=Copy]').should('be.visible')
+    })
+  },
+)
