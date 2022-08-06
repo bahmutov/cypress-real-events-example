71ecc3f41ac1eb57b47903e79dc9a9f6a892da52
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-06 11:53:14 -0400

    playing with the test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -21,11 +21,10 @@ it(
   },
 )
 
-it.only(
+it(
   'shows the copied tooltip',
   { browser: '!firefox', scrollBehavior: 'center' },
   () => {
-    cy.clock()
     cy.visit('dmtrKovalenko/cypress-real-events')
     cy.contains('h2', 'Installation').scrollIntoView()
 
@@ -36,6 +35,41 @@ it.only(
       // the SVG tooltip shows the text "Copied!"
       // using its ::after CSS content
       cy.get('.tooltipped').should('be.visible')
+      // then the tooltip goes away from the DOM completely
+      cy.get('.tooltipped').should('not.exist')
+    })
+  },
+)
+
+it(
+  'shows the tooltip with the expected text',
+  { browser: '!firefox', scrollBehavior: 'center' },
+  () => {
+    cy.visit('dmtrKovalenko/cypress-real-events')
+    cy.contains('h2', 'Installation').scrollIntoView()
+
+    cy.contains('.snippet-clipboard-content', 'npm install').within(() => {
+      cy.get('[aria-label=Copy]')
+        .should('not.be.visible')
+        .should('have.attr', 'data-copy-feedback')
+        .then((copyText) => {
+          cy.root().realHover()
+          cy.get('[aria-label=Copy]').should('be.visible').realClick()
+          // the SVG tooltip shows the text "Copied!"
+          // using its ::after CSS content
+          cy.get('.tooltipped')
+            .should('be.visible')
+            .then(($el) => {
+              const after = window.getComputedStyle($el[0], '::after')
+              const afterContent = after.getPropertyValue('content')
+              // the ::after content has double quotes
+              expect(afterContent, 'tooltip text').to.equal(
+                '"' + copyText + '"',
+              )
+            })
+          // then the tooltip goes away from the DOM completely
+          cy.get('.tooltipped').should('not.exist')
+        })
     })
   },
 )
