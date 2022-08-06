452361f6a9084242f15b06732ce5d76f8f2e0b66
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-06 08:03:36 -0400

    working on tooltip

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,7 +10,32 @@ it(
     cy.contains('.snippet-clipboard-content', 'npm install').within(() => {
       cy.get('[aria-label=Copy]').should('not.be.visible')
       cy.root().realHover()
-      cy.get('[aria-label=Copy]').should('be.visible')
+      cy.get('[aria-label=Copy]').should('be.visible').realClick()
+      // since the ".click" and ".realClick" do NOT retry
+      // to wait for the attribute to change, we need to query the element
+      // let's use the custom component <clipboard copy> element type
+      cy.get('clipboard-copy').should('have.attr', 'aria-label', 'Copied!')
+      // the "Copied" attribute reverts back
+      cy.get('clipboard-copy').should('have.attr', 'aria-label', 'Copy')
+    })
+  },
+)
+
+it.only(
+  'shows the copied tooltip',
+  { browser: '!firefox', scrollBehavior: 'center' },
+  () => {
+    cy.clock()
+    cy.visit('dmtrKovalenko/cypress-real-events')
+    cy.contains('h2', 'Installation').scrollIntoView()
+
+    cy.contains('.snippet-clipboard-content', 'npm install').within(() => {
+      cy.get('[aria-label=Copy]').should('not.be.visible')
+      cy.root().realHover()
+      cy.get('[aria-label=Copy]').should('be.visible').realClick()
+      // the SVG tooltip shows the text "Copied!"
+      // using its ::after CSS content
+      cy.get('.tooltipped').should('be.visible')
     })
   },
 )
